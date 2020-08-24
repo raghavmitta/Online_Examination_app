9ae17f0ddfdfb0939ad32cc24762ff7cbd7c8b3a
RAGHAV MITTAL <raghavmitta@gmail.com>
2020-08-24 19:47:11 +0530

    frontend-examengine

--- Frontend/online-examination-app/src/app/app.module.ts
+++ Frontend/online-examination-app/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExaminationInstructionComponent } from './examination-instruction/examination-instruction.component';
+import { LoadQuestionsComponent } from './load-questions/load-questions.component';
+import {FormsModule} from '@angular/forms'
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExaminationInstructionComponent,
+    LoadQuestionsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
